7ec4aeb96ae2bc17ee9d33dce80c8804b201d7ff
yanjankaf <socialyanjanendra@gmail.com>
2023-09-23 14:38:54 +0530

    Added Zipping Feature
    So now all the files will get zipped, before being available to
    download, code is messy, could have done better.
    
    Learnt a lot thanks for the opportunity, I did not know that there is a
    thing called import.meta LOL 😁
    
    So thanks, I'm not expecting any merge to master, as code can be
    improved, further.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,41 +2,50 @@ import path from "path";
 import reduceImageSize from "../utils/imageReducer.js";
 import deleteUploadedImage from "../utils/deleteUploadedImage.js";
 import fileDirName from "../utils/dirname.js";
+import { compressFiles } from "../utils/compressFiles.js";
+
 
 const { __dirname } = fileDirName(import.meta);
 
-export async function getHomePage(req, res){
+export async function getHomePage(req, res) {
     try {
         const HTMLFilePath = path.join(__dirname, '..', 'views', 'home.html')
         return res.sendFile(HTMLFilePath);
     } catch (e) {
         console.error(e);
-        res.status(500).send({ error: 'Server error'})
+        res.status(500).send({ error: 'Server error' })
     }
 }
 
-export async function postReduceImages(req, res){
+export async function postReduceImages(req, res) {
     const files = req.files;
 
-    const downloadLinks = [];
+    const beforeZipLinks = [];
     const asyncOperations = [];
 
     try {
         for (const file of files) {
             const { filename, originalname } = file;
             const uploadedFilePath = path.join(__dirname, '..', '..', 'uploads', filename);
-            const outputFilePath = path.join(__dirname, '..', '..', 'outputs', originalname);
+
+            const outputFilePath = path.join(__dirname, '..', '..', 'beforeZip', filename);
 
             await reduceImageSize(uploadedFilePath, outputFilePath);
             await deleteUploadedImage(uploadedFilePath);
-
-            downloadLinks.push(`/download/${originalname}`)
+            beforeZipLinks.push(
+                {
+                    fileName: filename,
+                    originalName: originalname,
+                });
         }
 
-        return res.send({ success: 'true', downloads: downloadLinks });
+        await compressFiles(beforeZipLinks, (download) => {
+            console.log(download);
+            return res.send({ success: 'true', downloads: [`/download/${download}`] });
+        });
     } catch (e) {
         console.error(e);
-        res.status(500).send({ error: 'Server error'})
+        res.status(500).send({ error: 'Server error' })
     }
 }
 
@@ -46,13 +55,13 @@ export async function getDownloadFileById(req, res) {
         const filePath = path.join(__dirname, '..', '..', 'outputs', filename);
 
         res.download(filePath, (e) => {
-            if(e) {
+            if (e) {
                 console.error(e);
                 res.status(500).send('Error downloading file');
             }
         })
     } catch (e) {
         console.error(e);
-        res.status(500).send({ error: 'Server error'})
+        res.status(500).send({ error: 'Server error' })
     }
 }
---
+++ src/utils/compressFiles.js
@@ -0,0 +1,48 @@
+import { createReadStream, createWriteStream, write } from "node:fs";
+import archiver from "archiver";
+import fileDirName from "./dirname.js";
+import { join } from "node:path";
+import { v4 } from "uuid";
+import clearDir from "./clearDir.js";
+
+/**
+ * @description This function will compress the files that has been reduced by sharp, and will give a zip file.
+ * @param {{fileName : String, originalName : String}[]} fileLinks 
+ */
+
+async function compressFiles(fileLinks, callback) {
+    const { __dirname } = fileDirName(import.meta);
+    const beforeZipPath = join(__dirname, "..", "..", "beforeZip");
+    const downloadFolder = join(__dirname, "..", "..", "outputs");
+
+    const zipName = `${v4()}.zip`;
+    const currentZipPath = join(downloadFolder, zipName);
+
+    const writeStream = createWriteStream(currentZipPath);
+
+    const archive = archiver('zip', {
+        zlib: { level: 9 }
+    });
+    archive.pipe(writeStream);
+
+    archive.on('error', function (err) {
+        throw err;
+    });
+    writeStream.on("error", (err) => {
+        throw err;
+    })
+
+    writeStream.on("close", ()=>{
+        clearDir(beforeZipPath);
+        callback(zipName);
+    })
+    for (const currentFile of fileLinks) {
+        archive.append(
+            createReadStream(join(beforeZipPath, currentFile.fileName)),
+            { name: currentFile.originalName }
+        );
+    };
+
+    await archive.finalize();
+}
+export { compressFiles };
